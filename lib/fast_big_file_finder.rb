6e02a7183f595437dad290fdd52328e11ddab4cb
Ben Snape <ben.snape@itv.com>
2013-01-07 11:56:52 +0000

    improved bash commands and removed some comments

--- lib/fast_big_file_finder.rb
+++ lib/fast_big_file_finder.rb
@@ -10,21 +10,17 @@ class FastBigFileFinder
 
   def fast_find_in_history(number=10)
     raise 'no packs found' if @git_lib.get_pack_number == 0
-    objects = `git verify-pack -v .git/objects/pack/pack-*.idx | grep -v objects | grep -v commit | sort -k4nr`
+    objects = `git verify-pack -v .git/objects/pack/pack-*.idx | grep -E 'tree|blob' | sort -k4nr`
 
     objects = objects.split("\n")
 
     test = {}
-    #p objects
 
     objects.each do |object|
       break if test.size == number
       sha = object.match(/^\w+/)[0]
       path = `git rev-list --all --objects | grep #{sha} | cut -f 2 -d ' '`.chomp
       # an empty path means you need to do a garbage collection
-      #puts "object: #{object}"
-      #puts "path: #{path}"
-      #puts "sha: #{sha}"
       next if File.exist?("#@repository/#{path}")
 
       # horrible regex because git verify-pack output can differ - http://www.kernel.org/pub/software/scm/git/docs/git-verify-pack.html
@@ -37,7 +33,7 @@ class FastBigFileFinder
 
   def fast_find_in_working_copy(number=10)
     raise 'no packs found' if @git_lib.get_pack_number == 0
-    objects = `git verify-pack -v .git/objects/pack/pack-*.idx | grep -v objects | grep -v commit | sort -k4nr ` #`| head -n #{number}`
+    objects = `git verify-pack -v .git/objects/pack/pack-*.idx | grep -E 'tree|blob' | sort -k4nr`
 
     objects = objects.split("\n")
 
