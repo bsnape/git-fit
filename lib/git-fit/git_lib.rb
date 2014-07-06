ed0c7825cbb0a8b2bee9806074ddaa80cbabd83b
Ben Snape <ben.snape@itv.com>
2014-07-06 23:11:23 +0100

    removed redundant methods

--- lib/git-fit/git_lib.rb
+++ lib/git-fit/git_lib.rb
@@ -9,22 +9,6 @@ module GitFit
       `git show -s --format=%at #{commit}`.strip.to_i
     end
 
-    def calculate_file_sizes
-      file_sizes = {}
-      get_file_list.each do |filepath|
-        file_sizes.merge!({ get_file_size(filepath) => filepath })
-      end
-      file_sizes
-    end
-
-    def get_file_size(path)
-      `wc -c #{path} | awk '{print $1}'`.strip.to_i
-    end
-
-    def get_object_sha_from_path(path)
-      `git ls-files -s #{path} | cut -d ' ' -f 2`
-    end
-
     def get_revision_list(commit)
       `git rev-list #{commit}`.split("\n")
     end
@@ -41,14 +25,6 @@ module GitFit
           :path => path
         }
       end
-    end
-
-    def get_file_list
-      `git ls-files -z`.split("\u0000")
-    end
-
-    def get_commit_count_for_branch(branch)
-      `git rev-list --branches #{branch} | wc -l`.strip.to_i
     end
 
     def get_all_blobs
--- spec/git_commands_spec.rb
+++ spec/git_commands_spec.rb
@@ -11,10 +11,6 @@ describe 'git commands' do
     Dir.chdir @cloned_repo_path
   end
 
-  it "should list all the file paths in the repository's working copy" do
-    @git_lib.get_file_list.should match_array ['test.txt', 'test/blah/hello.txt']
-  end
-
   it 'should get the commit author for a given commit' do
     @git_lib.get_commit_author('25097b1').should eq 'Ben Snape'
   end
@@ -27,19 +23,6 @@ describe 'git commands' do
   it 'should get the revision list for a given commit' do
     expected_revision_list = ['25097b13910521f43a3bbc5236973bcc2a06093b', 'b224a986d123ae3718b1e8762e596b111a4367e0']
     @git_lib.get_revision_list('HEAD').should match_array expected_revision_list
-  end
-
-  it 'should get the commit count for a given branch' do
-    @git_lib.get_commit_count_for_branch('master').should eq 2
-  end
-
-  it 'should return the size of a file in bytes for a given filepath' do
-    @git_lib.get_file_size('test/blah/hello.txt').should eq 7
-  end
-
-  it 'should calculate the size of all files in the working copy' do
-    expected_file_sizes = { 2307 => 'test.txt', 7 => 'test/blah/hello.txt' }
-    @git_lib.calculate_file_sizes.should == expected_file_sizes
   end
 
   it 'should get the contents of a commit object' do
